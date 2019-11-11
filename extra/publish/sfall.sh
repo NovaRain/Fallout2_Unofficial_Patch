208bc84b3defabb65afcdfa4b930d4c89b20c676
Magus <magus@bgforge.net>
2019-11-11 18:05:27 +0700

    always use latest ddraw.ini, apply UP changed settings on top

--- extra/publish/sfall.sh
+++ extra/publish/sfall.sh
@@ -3,10 +3,18 @@
 set -xeu -o pipefail
 
 release_dir="$(realpath $release_dir)"
+extra_dir="$(realpath $extra_dir)"
+release_ini="$release_dir/ddraw.ini"
+up_ini="$extra_dir/publish/ddraw.ini"
 
 sfall_url="https://sourceforge.net/projects/sfall/files/sfall/sfall_$sfall_version.7z/download"
 wget -q "$sfall_url" -O sfall.7z
-for f in ddraw.dll sfall.dat; do
+for f in ddraw.dll ddraw.ini sfall.dat; do
   7zr e sfall.7z "$f"
   mv "$f" "$release_dir/"
 done
+
+crudini --merge "$release_ini" < "$up_ini"
+version="$(echo $TRAVIS_TAG | tr 'v' 'u')"
+crudini --set "$release_ini" "Misc" "VersionString" "FALLOUT II 1.02.31u${version}"
+unix2dos "$release_ini"
