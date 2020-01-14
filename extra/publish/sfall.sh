d8d309be52d2baffd2c22a6d38148ab6d1ce4d92
Magus <magus@bgforge.net>
2020-01-14 13:35:13 +0700

    cosmetic

--- extra/publish/sfall.sh
+++ extra/publish/sfall.sh
@@ -5,7 +5,7 @@ set -xeu -o pipefail
 release_dir="$(realpath $release_dir)"
 extra_dir="$(realpath $extra_dir)"
 release_ini="$release_dir/ddraw.ini"
-up_ini="$extra_dir/publish/ddraw.ini"
+custom_ini="$extra_dir/publish/ddraw.ini"
 
 sfall_url="https://sourceforge.net/projects/sfall/files/sfall/sfall_$sfall_version.7z/download"
 wget -q "$sfall_url" -O sfall.7z
@@ -14,7 +14,7 @@ for f in ddraw.dll ddraw.ini sfall.dat; do
   mv "$f" "$release_dir/"
 done
 
-crudini --merge "$release_ini" < "$up_ini"
+crudini --merge "$release_ini" < "$custom_ini"
 version="$(echo $TRAVIS_TAG | tr 'v' 'u')"
 crudini --set "$release_ini" "Misc" "VersionString" "FALLOUT II 1.02.31${version}"
 unix2dos "$release_ini"
