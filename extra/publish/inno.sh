1f01b02396636a9a594f11c5b339a478d4858f1d
Magus <magus@bgforge.net>
2020-03-04 02:20:06 +0700

    inno #39: trying to get working

--- extra/publish/inno.sh
+++ extra/publish/inno.sh
@@ -6,14 +6,15 @@ bin_dir="$(realpath $bin_dir)"
 mods_dir="$(realpath $mods_dir)"
 release_dir="$(realpath release_dir)"
 install_iss="$extra_dir/inno/inno.iss"
-inno_dir="C:\programs\inno"
+inno_dir=~/.wine/drive_c/programs/inno
 inno_bin="$inno_dir/ISCC.exe"
 inno_install_bin="is.exe"
+inno_install_dir="C:\programs\inno"
 inno_url="https://jrsoftware.org/download.php/$inno_install_bin"
 
 #install innosetup
 wget -q "$inno_url"
-wine "$inno_install_bin" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /DIR="$inno_dir"
+wine "$inno_install_bin" /SILENT /SUPPRESSMSGBOXES /NORESTART /DIR="$inno_install_dir"
 rm -f "$inno_install_bin"
 
 # delete unnecessary files
