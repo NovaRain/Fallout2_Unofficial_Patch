4b5c28fbf8a200186e0577f4d4df5bf6fdea8258
Magus <magus@bgforge.net>
2020-03-04 01:49:49 +0700

    innosetup instead of nsis: #39

---
+++ extra/publish/inno.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -xeu -o pipefail
+
+bin_dir="$(realpath $bin_dir)"
+mods_dir="$(realpath $mods_dir)"
+release_dir="$(realpath release_dir)"
+install_iss="$extra_dir/inno/inno.iss"
+inno_dir="C:\programs\inno"
+inno_bin="$inno_dir/ISCC.exe"
+inno_install_bin="is.exe"
+inno_url="https://jrsoftware.org/download.php/$inno_install_bin"
+
+#install innosetup
+wine "$inno_install_bin" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /DIR="$inno_dir"
+rm -f "$inno_install_bin"
+
+# delete unnecessary files
+rm -f "$release_dir"/{upu-install.sh,upu-install.command}
+sed -i "s|define uversion .*|define uversion \"${uversion}\"|" "$install_iss"
+sed -i "s|define vversion .*|define vversion \"${vversion}\"|" "$install_iss"
+wine "$inno_bin" "$install_iss"
+mv "$extra_dir"/inno/Output/*.exe .
