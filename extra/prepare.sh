c60ec93fb0bae7e5ad169f92f44b6d46be7e74c1
Magus <magus@bgforge.net>
2020-04-05 01:18:26 +0700

    try adding more deps

--- extra/prepare.sh
+++ extra/prepare.sh
@@ -27,5 +27,6 @@ mv "scripting_docs/headers" "scripts_src/sfall"
 sudo dpkg --add-architecture i386
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
-sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+sudo add-apt-repository ppa:cybermax-dexter/sdl2-backport
 sudo apt install winehq-stable
