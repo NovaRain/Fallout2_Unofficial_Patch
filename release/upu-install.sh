563217c5bd8b3f6707379df45d4f705ab0bd125c
Magus <magus@bgforge.net>
2020-06-26 22:39:26 +0700

    only keep music in data

--- release/upu-install.sh
+++ release/upu-install.sh
@@ -3,20 +3,24 @@
 cd -- "$(dirname "$BASH_SOURCE")"
 
 bdir="backup/upu"
-mkdir -p $bdir/data
+mkdir -p $bdir/data/sound
 
 if [[ -f patch000.dat ]]; then
-  mv patch000.dat $bdir/
+  mv patch000.dat "$bdir"/
 fi
 
 # keep savegames, sound, character files
 for i in $(ls data | grep -vi "^savegame$\|\.txt$\|\.gcd$\|^sound$"); do
-  mv data/$i $bdir/data/
+  mv data/"$i" "$bdir"/data/
+done
+# only need music from sound
+for i in $(ls data/sound | grep -vi "^music$"); do
+  mv data/"$i" "$bdir"/data/sound/
 done
 
 # sound path fix
 rm -f camlrt.dll initool.exe
-cp fallout2.cfg $bdir/
+cp fallout2.cfg "$bdir"/
 sed -i.bak 's|music_path1=.*|music_path1=data\\sound\\music\\|' fallout2.cfg
 sed -i.bak 's|music_path2=.*|music_path2=sound\\music\\|' fallout2.cfg
 rm -f fallout2.cfg.bak
