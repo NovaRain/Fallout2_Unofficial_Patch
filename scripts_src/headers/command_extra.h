0e2e87cfde5c8903d6eae49efb60a1c54b12dfe9
Magus <magus@bgforge.net>
2021-01-05 04:08:59 +0700

    sync with RPU, ref https://github.com/BGforgeNet/Fallout2_Restoration_Project/issues/72

---
+++ scripts_src/headers/command_extra.h
@@ -0,0 +1,16 @@
+// This file is for command.g differences between UP and RP.
+// Its purpose is to keep actual command.h identical between UP and RP, easing maintenance.
+// Non-command.h additions should go to rpu.h.
+
+#ifndef COMMAND_EXTRA_H
+#define COMMAND_EXTRA_H
+
+#define protected_from_gas(x) ( \
+ (obj_pid(get_armor(x)) == PID_POWERED_ARMOR) \
+  or (obj_pid(get_armor(x)) == PID_HARDENED_POWER_ARMOR) \
+  or (obj_pid(get_armor(x)) == PID_ADVANCED_POWER_ARMOR) \
+  or (obj_pid(get_armor(x)) == PID_ADVANCED_POWER_ARMOR_MK2) \
+  or (obj_pid(get_armor(x)) == PID_ENVIRONMENTAL_ARMOUR) \
+)
+
+#endif  // COMMAND_EXTRA_H
--- scripts_src/headers/rpu_command.h
+++
@@ -1,3 +0,0 @@
-// This file is for RP additions to command.h.
-// Its purpose is to keep actual command.h identical between UP and RP, easing maintenance.
-// It must be empty in UP.
