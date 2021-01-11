385a9baae9571f0093d5f6960112952c6e9fecbc
Magus <magus@bgforge.net>
2021-01-11 19:40:51 +0700

    accomodate doors with different script names #61

--- scripts_src/headers/doors2.h
+++ scripts_src/headers/doors2.h
@@ -1,3 +1,7 @@
+#ifndef door_mstr
+   #define door_mstr(x)                    message_str(SCRIPT_DOOR, x)
+#endif
+
 /**************************************************************************************
    Should the trap go off for any reason by critter influence, then this procedure will
    be called to deal damage to the critter, display a message stating how much damage
@@ -12,13 +16,13 @@ procedure Damage_Critter begin
    if (source_obj == dude_obj) then begin
        critter_dmg(dude_obj,Trap_Damage,(DMG_explosion BWOR DMG_BYPASS_ARMOR));
 /* display_msg("You set off the trap and were hit for "+Trap_Damage+" points of damage.);                 */
-       display_msg(mstr(166)+Trap_Damage+mstr(167));
+       display_msg(door_mstr(166)+Trap_Damage+door_mstr(167));
    end
 
    else begin
        critter_dmg(source_obj,Trap_Damage,(DMG_explosion BWOR DMG_BYPASS_ARMOR));
 /* display_msg(Critter_Name+" set off the trap was hit for "+Trap_Damage+" points of damage");        */
-       display_msg(obj_name(source_obj)+mstr(168)+Trap_Damage+mstr(169));
+       display_msg(obj_name(source_obj)+door_mstr(168)+Trap_Damage+door_mstr(169));
    end
 
 /* The trap is now disarmed and should never go off again.                           */
