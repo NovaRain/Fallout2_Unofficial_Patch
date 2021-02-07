ef8c89a5e855789770a618f1d76fc97e4f46c084
Magus <magus@bgforge.net>
2021-02-07 18:26:45 +0700

    removed legacy build files

--- scripts_src/main/main.bat
+++
@@ -1,7 +0,0 @@
-call ..\p AHELDER
-call ..\p AHHAKUN
-call ..\p NHMYRON
-call ..\p OBJ_DUDE
-call ..\p QHPRZRCH
-call ..\p SHTANDI
-call ..\p VCLYNETT
--- scripts_src/raiders/raiders.bat
+++
@@ -1,8 +0,0 @@
-call ..\p ICMERC
-call ..\p ICMRCCPT
-call ..\p ICSCOUT
-call ..\p IILOCKDR
-call ..\p IIMINE
-call ..\p IIPIT
-call ..\p IISAFE
-call ..\p IISCORP
