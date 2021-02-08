fd2bd0c47ddad0ddd6a9ee08352b8e5ad506f287
Magus <magus@bgforge.net>
2021-02-08 13:47:00 +0700

    use docker for inno

--- extra/package/inno.sh
+++ extra/package/inno.sh
@@ -3,25 +3,10 @@
 set -xeu -o pipefail
 
 install_iss="extra/inno/inno.iss" # inno doesn't like absolute path
-inno_dir=~/.wine/drive_c/programs/inno
-inno_bin="$inno_dir/ISCC.exe"
-inno_install_bin="is.exe"
-inno_install_dir="C:\programs\inno"
-inno_url="https://jrsoftware.org/download.php/$inno_install_bin"
-
-#install innosetup
-if [[ ! -f $inno_bin ]]; then
-  wget -nv "$inno_url"
-  set +e
-  wine "$inno_install_bin" /SP- /NORESTART /LOG="inno.log" /DIR="$inno_install_dir" || true
-  cat inno.log
-  exit 1
-  rm -f "$inno_install_bin"
-fi
 
 # delete unnecessary files
 rm -f "$release_dir"/{upu-install.sh,upu-install.command}
 sed -i "s|define uversion .*|define uversion \"${uversion}\"|" "$install_iss"
 sed -i "s|define vversion .*|define vversion \"${vversion}\"|" "$install_iss"
-wine "$inno_bin" "$install_iss"
+sudo docker run --rm -i -v $PWD:/work amake/innosetup "$install_iss"
 mv "$extra_dir"/inno/Output/*.exe .
