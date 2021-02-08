5e0deb31c87a50a62d4faaa5c378de6abea191b1
Magus <magus@bgforge.net>
2021-02-08 14:12:27 +0700

    maybe there's docker already

--- extra/package/inno.sh
+++ extra/package/inno.sh
@@ -8,5 +8,5 @@ install_iss="extra/inno/inno.iss" # inno doesn't like absolute path
 rm -f "$release_dir"/{upu-install.sh,upu-install.command}
 sed -i "s|define uversion .*|define uversion \"${uversion}\"|" "$install_iss"
 sed -i "s|define vversion .*|define vversion \"${vversion}\"|" "$install_iss"
-sudo docker run --rm -i -v $PWD:/work amake/innosetup "$install_iss"
+docker run --rm -i -v $PWD:/work amake/innosetup "$install_iss"
 mv "$extra_dir"/inno/Output/*.exe .
