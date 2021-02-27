b60e91a9ac218533498cf959217f6ec21d04f7a3
Magus <magus@bgforge.net>
2021-02-28 04:19:05 +0700

    use simpler names for title functions, account for evil critters #4

--- scripts_src/headers/upu.h
+++ scripts_src/headers/upu.h
@@ -57,22 +57,75 @@ end
 #define dude_has_cult has_trait(TRAIT_PERK, dude_obj, PERK_cult_of_personality)
 
 // effective reputation for critter reaction or lines (Reply). NOT for dude responses (NOption, etc).
+// Evil_Critter is defined in talk_p_proc, while check_general_rep happens in other procedures too, so we build a separate array for them.
+procedure self_is_evil begin
+  variable evil_critters = [
+    // grep -R "Evil_Critter:=1" scripts_src/ | awk -F ':' '{print $1}' | xargs grep "#define SCRIPT_REALNAME" | awk '{print $3}' | sort
+    // Can't use SCRIPT_NAME because it's defined in (parentheses) and sfall array syntax won't have it.
+    "bcdardog",
+    "bcdargrd",
+    "bcdarion",
+    "bcgengrd",
+    "bckarla",
+    "bcphil",
+    "fcdaveh",
+    "fcelrind",
+    "fcjuavki",
+    "fclopan",
+    "fcoz7",
+    "fcoz9",
+    "ncbismen",
+    "ncdrgdlr",
+    "ncmormen",
+    "ncramire",
+    "ncsalmen",
+    "ocmatt",
+    "qcfrank",
+    "rchakes",
+    "scbuster",
+    "sclenny",
+    "scmerk",
+    "scmira",
+    "scmrkgrd",
+    "scoswald",
+    "scrawpat",
+    "scskeete",
+    "scslvgrd",
+    "scvortis"
+  ];
+  if is_in_array(SCRIPT_REALNAME, evil_critters) then return true;
+  return false;
+end
+
+// Negative rep is liked by evil critters
 procedure effective_rep begin
-  variable rep := global_var(GVAR_PLAYER_REPUTATION);
-  if has_trait(TRAIT_PERK, dude_obj, PERK_karma_beacon_perk) then rep := rep * 2;
-  if dude_has_cult and rep < 0 then rep = -1 * rep;
+  variable rep = global_var(GVAR_PLAYER_REPUTATION);
+  if has_trait(TRAIT_PERK, dude_obj, PERK_karma_beacon_perk) then rep = rep * 2;
+  if dude_has_cult then begin
+    rep = abs(rep);
+    if self_is_evil then return -rep;
+    else return rep;
+  end
   return rep;
 end
 
-procedure title_better_or_equal(variable title) begin
+#define rep_positive (effective_rep > 0)
+#define rep_negative (effective_rep < 0)
+
+// normal mode: critter likes dude if he has title, taking Evil_Critter into account
+procedure check_title(variable title) begin
   variable rep := effective_rep;
-  if rep >= title then return true;
+  if not self_is_evil and rep >= title then return true;
+  if self_is_evil == 1 and rep <= title then return true;
   return false;
 end
-procedure title_worse_or_equal(variable title) begin
+// inverse mode: critter dislikes dude if he has title, taking Evil_Critter into account
+procedure check_title_bad(variable title) begin
   variable rep := effective_rep;
-  if rep <= title then return true;
-  return false;
+  if not self_is_evil and rep >= title then return false;
+  if self_is_evil and rep <= title then return false;
+  return true;
 end
+
 
 #endif  // UPU_H
