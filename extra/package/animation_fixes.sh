72cf0d823f2ed2cab3166dbf528cb62468c43caa
Magus <magus@bgforge.net>
2023-01-27 09:45:23 +0700

    not sure why the build keeps failing

--- extra/package/animation_fixes.sh
+++ extra/package/animation_fixes.sh
@@ -8,5 +8,6 @@ export optional_dir="optional"
 mkdir -p "$optional_dir"
 cd "$optional_dir"
 for f in goris_fast_derobing_low_fps.dat walk_speed_fix_low_fps.dat; do
-  wget -nv "$(curl -s $api_url | grep browser_download_url | grep $f | awk -F '"' '{print $4}')"
+  url="$(curl -s "$api_url" | grep browser_download_url | grep $f | awk -F '"' '{print $4}')"
+  wget -nv "$url"
 done
