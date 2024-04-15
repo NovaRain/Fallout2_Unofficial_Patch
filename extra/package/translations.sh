3ed9dfac4a3075ed5337d81b57012787ad326786
Magus <magus@bgforge.net>
2024-04-15 15:23:21 +0700

    formatting for scripts, add more mods to order file

--- extra/package/translations.sh
+++ extra/package/translations.sh
@@ -12,13 +12,13 @@ rm -f "$lang_dir/english/cuts/intro.sve"
 # update patchinf
 # shellcheck disable=SC2045  # Let it break if there are weird filenames.
 for lang in $(ls $lang_dir); do
-  patchinf="$lang_dir/$lang/dialog/patchinf.msg"
-  # display current version
-  # shellcheck disable=SC2154  # from package.sh
-  sed -i "s|1\.02\.31u[0-9]*\.|1.02.31${uversion}.|" "$patchinf"
-  # if the string is untranslated, still show correct language
-  lang_name="${lang^}"
-  sed -i "s|English|$lang_name|" "$patchinf"
+    patchinf="$lang_dir/$lang/dialog/patchinf.msg"
+    # display current version
+    # shellcheck disable=SC2154  # from package.sh
+    sed -i "s|1\.02\.31u[0-9]*\.|1.02.31${uversion}.|" "$patchinf"
+    # if the string is untranslated, still show correct language
+    lang_name="${lang^}"
+    sed -i "s|English|$lang_name|" "$patchinf"
 done
 
 # duplicate cuts for english language, otherwise sfall won't display them in english
@@ -27,24 +27,24 @@ cp -r "$lang_dir"/english/cuts{,_female}
 # move texts to translation package dir
 # shellcheck disable=SC2010  # shouldn't have non-alphanumeric filenames here
 for d in $(ls $lang_dir | grep -v english); do
-  rm -rf "$trans_dir/$d/text"
-  mkdir -p "$trans_dir/$d/text"
-  mv "$lang_dir/$d" "$trans_dir/$d/text/"
+    rm -rf "$trans_dir/$d/text"
+    mkdir -p "$trans_dir/$d/text"
+    mv "$lang_dir/$d" "$trans_dir/$d/text/"
 done
 
 # package into dats
 cd "$trans_dir"
 rm -f ./*.dat
 for d in *; do
-  # shellcheck disable=SC2154  # from env.sh
-  dat="${mod_name}_$d.dat"
-  cd "$d"
-  # shellcheck disable=SC2154  # from package.sh
-  find . -type f | sed -e 's|^\.\/||' -e 's|\/|\\|g' | sort >"$file_list"
-  # shellcheck disable=SC2154  # from package.sh
-  $dat2a "$dat" @"$file_list"
-  mv "$dat" ..
-  cd ..
+    # shellcheck disable=SC2154  # from env.sh
+    dat="${mod_name}_$d.dat"
+    cd "$d"
+    # shellcheck disable=SC2154  # from package.sh
+    find . -type f | sed -e 's|^\.\/||' -e 's|\/|\\|g' | sort >"$file_list"
+    # shellcheck disable=SC2154  # from package.sh
+    $dat2a "$dat" @"$file_list"
+    mv "$dat" ..
+    cd ..
 done
 cd ..
 
