93b617ced69d5c48e43b59bd5a8f625d78b21e1e
Magus <magus@bgforge.net>
2024-04-15 15:38:43 +0700

    formatting and typos

--- release/upu-install.sh
+++ release/upu-install.sh
@@ -6,49 +6,49 @@ cd -- "$(dirname "$0")"
 
 # is FS case sensitive?
 touch fs_testx fs_testX
-# shellcheck disable=SC2012  # Don't have non-alphanumberic filenames.
+# shellcheck disable=SC2012  # Don't have non-alphanumeric filenames.
 if [[ "$(ls fs_test* | wc -l)" == "2" ]]; then
-  rm -f fs_testx fs_testX
-  if [[ "$(find . -name "[[:upper:]]*" | grep -vc "mods/AmmoGlovz.ini\|mods/AmmoYAAM.ini")" != "0" ]]; then
-    echo "The filesystem is case sensitive. You must recursively lowercase Fallout game directory before proceeding."
-    exit 1
-  fi
+    rm -f fs_testx fs_testX
+    if [[ "$(find . -name "[[:upper:]]*" | grep -vc "mods/AmmoGlovz.ini\|mods/AmmoYAAM.ini")" != "0" ]]; then
+        echo "The filesystem is case sensitive. You must recursively lowercase Fallout game directory before proceeding."
+        exit 1
+    fi
 else
-  rm -f fs_testx
+    rm -f fs_testx
 fi
 
 if [[ ! -f master.dat ]]; then
-  echo "File master.dat not found. This is not Fallout 2 directory. Aborting."
-  exit 1
+    echo "File master.dat not found. This is not Fallout 2 directory. Aborting."
+    exit 1
 fi
 if [[ -f data/scripts/glkagako.int ]]; then
-  echo "RP installation detected. UPU cannot be installed on RP. Aborting. Re-start with a fresh Fallout 2 installation."
-  exit 1
+    echo "RP installation detected. UPU cannot be installed on RP. Aborting. Re-start with a fresh Fallout 2 installation."
+    exit 1
 fi
 if [[ -f mods/rpu.dat ]]; then
-  echo "RPU installation detected. UPU cannot be installed on RPU. Aborting. Re-start with a fresh Fallout 2 installation."
-  exit 1
+    echo "RPU installation detected. UPU cannot be installed on RPU. Aborting. Re-start with a fresh Fallout 2 installation."
+    exit 1
 fi
 
 bdir="backup/upu"
 mkdir -p "$bdir"/data/sound
 
 if [[ -f patch000.dat ]]; then
-  mv patch000.dat "$bdir"/
+    mv patch000.dat "$bdir"/
 fi
 if [[ -f f2_res.dat ]]; then
-  mv f2_res.dat "$bdir"/
+    mv f2_res.dat "$bdir"/
 fi
 
 # keep savegames, character files, sound
-# shellcheck disable=SC2010  # Don't have non-alphanumberic filenames.
+# shellcheck disable=SC2010  # Don't have non-alphanumeric filenames.
 for i in $(ls data | grep -vi "^savegame$\|\.txt$\|\.gcd$\|^sound$"); do
-  mv data/"$i" "$bdir"/data/
+    mv data/"$i" "$bdir"/data/
 done
 # only need music from sound
-# shellcheck disable=SC2010  # Don't have non-alphanumberic filenames.
+# shellcheck disable=SC2010  # Don't have non-alphanumeric filenames.
 for i in $(ls data/sound | grep -vi "^music$"); do
-  mv data/sound/"$i" "$bdir"/data/sound/
+    mv data/sound/"$i" "$bdir"/data/sound/
 done
 
 # sfall mods
