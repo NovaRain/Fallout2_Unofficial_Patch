fd63927731793f086ad90bab971271c7fee9497b
NovaRain <novarain@gmail.com>
2024-09-02 15:06:16 +0800

    Some refactoring and fixes for zcbrahmn:
    * Use constants instead of values for timer events.
    * Make sure downed brahmin can stand up when re-entering the map.
    * Check brahmin's state before calling stand up anim sequence.
    * Use ANIM_back_to_standing instead reverse-playing fall back anim
    (more naturally and doesn't cause recovered brahmin to look "frozen".)
    * Now all types of alcohol can be used on brahmin.
    
    * Will need to review/sync other brahmn scripts.

--- scripts_src/headers/command_extra.h
+++ scripts_src/headers/command_extra.h
@@ -5,6 +5,14 @@
 #ifndef COMMAND_EXTRA_H
 #define COMMAND_EXTRA_H
 
+#define is_alcohol(x) ( \
+ (obj_pid(x) == PID_BEER) \
+  or (obj_pid(x) == PID_BOOZE) \
+  or (obj_pid(x) == PID_GAMMA_GULP_BEER) \
+  or (obj_pid(x) == PID_ROENTGEN_RUM) \
+  or (obj_pid(x) == PID_ROT_GUT) \
+)
+
 #define protected_from_gas(x) ( \
  (obj_pid(get_armor(x)) == PID_POWERED_ARMOR) \
   or (obj_pid(get_armor(x)) == PID_HARDENED_POWER_ARMOR) \
