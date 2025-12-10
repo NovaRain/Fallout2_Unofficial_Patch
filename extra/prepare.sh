b74bd9d0f7c792dd84aa49a0a482e8fb0172810a
Magus <magus@bgforge.net>
2025-12-10 17:08:30 +0700

    Fix missing sslc download

--- extra/prepare.sh
+++ extra/prepare.sh
@@ -7,6 +7,12 @@ mpack_file="modderspack_$mpack_version.7z"
 mpack_url="https://sourceforge.net/projects/sfall/files/Modders%20pack/$mpack_file/download"
 sfall_headers_dir="scripts_src/sfall"
 
+# Download sslc compiler
+if [[ ! -f "$COMPILE" ]]; then
+    wget -q "$SSLC_URL" -O "$COMPILE"
+    chmod +x "$COMPILE"
+fi
+
 # shellcheck disable=SC2154  # from env.sh
 wget -nv "$mpack_url" -O "$mpack_7z"
 # sfall headers
